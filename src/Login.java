1f4479b130d1c8af67e02032b4094b2d392a5e48
Ricardo Rezende <rickrmb@hotmail.com>
2021-05-28 19:40:50 -0300

    criando método de login

---
+++ src/Login.java
@@ -0,0 +1,43 @@
+
+public class Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String emailDigitado = "admin@fiap.com.br";
+		String senhaDigitada = "123456";
+		String tokenDigitado = "beatriz";
+		
+		boolean loginOK = validarLogin(emailDigitado, senhaDigitada, tokenDigitado);
+		
+		System.out.println("loginOK");
+	
+	}
+		
+	
+	
+	
+		
+	public static boolean validarLogin(String email, String token, String senha) {
+		
+		boolean retorno = false;
+		
+		if (email.equals("admin@fiap.com.br") && senha.equals("123456")) {
+			retorno = true;
+			
+		}
+		
+		
+		
+		return retorno;
+	}
+
+}
+
+
+
+/*
+modificadorAcesso palavraChave tipoRetorno nomeFuncao(parametros){
+	processo
+	retorno	
+}*/
